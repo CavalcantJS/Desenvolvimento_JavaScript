8dd9c714b8ce38df8e85f3d5b757080b336e3db1
Jonathan Cavalcante <jonathancavalcante1911@gmail.com>
2025-08-07 13:52:31 -0300

    .

---
+++ Aula 8 - introdução JS/script.js
@@ -0,0 +1,12 @@
+// Dado do tipo string
+console.log("hello world");
+console.log("olá mundo");
+
+// Dado do tipo number
+console.log(2+2);
+
+//dado do tipo boolean
+console.log(true);
+console.log(false);
+
+
---
+++ Aula 8 - introdução JS/tabuada.js
@@ -0,0 +1,12 @@
+console.log("Tabuada do 5");
+
+console.log("5 X 1 é " +5*1)
+console.log("5 X 2 é " +5*2)
+console.log("5 X 3 é " +5*3)
+console.log("5 X 4 é " +5*4)
+console.log("5 X 5 é " +5*5)
+console.log("5 X 6 é " +5*6)
+console.log("5 X 7 é " +5*7)
+console.log("5 X 8 é " +5*8)
+console.log("5 X 9 é " +5*9)
+console.log("5 X 10 é " +5*10)
