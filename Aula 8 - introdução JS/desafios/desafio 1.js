3f088dab58fc9bc57c9c26f9ec06217522ef16fb
Jonathan Cavalcante <jonathancavalcante1911@gmail.com>
2025-08-07 17:05:21 -0300

    .

---
+++ Aula 8 - introdução JS/desafios/desafio 1.js
@@ -0,0 +1,6 @@
+ let num1 = 7
+let ant = num1 - 1
+ let suc = num1 + 1
+
+console.log("o antecessor de ", num1, "é:", ant, " e seu sucessor é:", suc);
+
---
+++ Aula 8 - introdução JS/desafios/desafio 2.js
@@ -0,0 +1,10 @@
+
+let num2= 49
+
+let dobro= 49*2
+let triplo = 49*3
+
+let rad = Math.sqrt(num2)
+
+console.log("o dobro de ", num2, "é:", dobro, "o triplo é", triplo, "e a raiz quadrada é", rad);
+
