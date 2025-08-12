a716d69273fce2d68d429a2ef8f0162c963c9418
Jonathan Cavalcante <jonathancavalcante1911@gmail.com>
2025-08-12 16:56:34 -0300

    .

---
+++ Aula 11 - Condicoes desencadeadas/exemplos/media.js
@@ -0,0 +1,18 @@
+let nota1 = 30
+let nota2 = 30
+
+let media = (nota1 + nota2)/2
+
+if (media >= 70){
+
+    console.log("Aprovado")
+} else if(media >= 50){
+
+    console.log("Recuperação");
+    
+
+}else{
+
+    console.log("Reprovado");
+    
+}
