8cb72d681753f74d1f03b1599f6247c76be2e567
Jonathan Cavalcante <jonathancavalcante1911@gmail.com>
2025-08-13 16:51:50 -0300

    .

--- Aula 11 - Condicoes desencadeadas/metodospagamento.js
+++ Aula 11 - Condicoes desencadeadas/metodospagamento.js
@@ -1,28 +1,42 @@
-// Valor gasto pelo usuario.
-// Metodo de pagamento
-//   - 1 - Boleto/Pix = 10% desconto
-//   - 2 - Credito = Valor total  
+// 1 - Pix
+// 2 - Crédito
+// 3- Débito    
+    // 1 - Parcela - normal
+    // 2 - Parcela - 5% juros
+    // 3 - Parcela - 10% juros
 
 
 let valorGasto = 1000
-let metodoPagamento = 1
-
+let metodoPagamento = 2
+let parcela = 3
 
 if (metodoPagamento == 1) {
     let valorFinal = valorGasto * 0.9
     console.log("MÉTODO DE PAGAMENTO BOLETO/PIX")
     console.log("O valor final foi de R$ " + valorFinal);
 }else if(metodoPagamento == 2){
-    console.log("MÉTODO DE PAGAMENTO CRÉDTIO")
-    console.log("O valor final foi de R$" + valorGasto);
+    if (parcela == 1) {
+        console.log("O valor final foi de R$" + valorGasto);
+    } else if (parcela == 2) {
+        let parcela = valorGasto * 1.05
+            console.log("O valor final foi de R$" + parcela);
+    } else {
+        let parcela = valorGasto * 1.1
+            console.log("O valor final foi de R$" + parcela);
 
-
+    }
+    
 } else if(metodoPagamento ==3){
-     let valorFinal = valorGasto * 0.5
-    console.log("MÉTODO DE PAGAMENTO CRÉDITO")
+     let valorFinal = valorGasto * 0.95
+    console.log("MÉTODO DE PAGAMENTO DÉBITO")
     console.log("O valor final foi de R$ " + valorFinal);
+
+
 
 }else {
     console.log("ESCOLHA UMA OPÇÃO VÁLIDA")
-    return
+    
 }
+
+
+
